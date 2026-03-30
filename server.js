9a974349befaef970c0593863b6a40868d8da18c
matlav <mathis.laversin92@gmail.com>
2026-03-30 14:53:22 +0200

    add var env

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const http = require("http");
+const http = require('http');
 
 const port = process.env.PORT || 3000;
+const customVar = process.env.MY_CUSTOM_VAR || 'not set';
 
 http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Azure App Service!");
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`Hello from Azure App Service! MY_CUSTOM_VAR = ${customVar}`);
 }).listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
